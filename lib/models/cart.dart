f78569a3b84afa558063d0000c9ade1b92fa8b1b
Jooaomarcelo <joaomdsb@hotmail.com>
2025-01-10 16:43:43 -0300

    added item_cart and cart models

---
+++ lib/models/cart.dart
@@ -0,0 +1,60 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:shop/models/cart_item.dart';
+import 'package:shop/models/product.dart';
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items => {..._items};
+
+  int itemsCount() {
+    return _items.length;
+  }
+
+  void addItem(Product product) {
+    if (_items.containsKey(product.id)) {
+      _items.update(
+        product.id,
+        (currentItem) => CartItem(
+          id: currentItem.id,
+          productId: currentItem.productId,
+          name: currentItem.name,
+          quantity: currentItem.quantity + 1,
+          price: currentItem.price,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        product.id,
+        () => CartItem(
+          id: Random().nextDouble().toString(),
+          productId: product.id,
+          name: product.name,
+          quantity: 1,
+          price: product.price,
+        ),
+      );
+    }
+    notifyListeners();
+  }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+  void clearCart() {
+    _items = {};
+    notifyListeners();
+  }
+
+  double getTotalAmount() {
+    double totalAmount = _items.values
+        .map((cartItem) => cartItem.price * cartItem.quantity)
+        .reduce((sum, element) => sum + element);
+
+    return totalAmount;
+  }
+}
