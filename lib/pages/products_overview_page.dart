28c00829ba610996a39b639c020962a4e0f8caf3
Jooaomarcelo <joaomdsb@hotmail.com>
2025-01-10 12:17:40 -0300

    extracted products grid from home page into a separated component

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,34 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../models/product_list.dart';
-import '../components/product_item.dart';
-import '../models/product.dart';
+import '../components/product_grid.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-
-    final List<Product> loadedProducts = provider.items;
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Loja'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemCount: loadedProducts.length,
-        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
-      ),
+      body: const ProductGrid(),
     );
   }
 }
