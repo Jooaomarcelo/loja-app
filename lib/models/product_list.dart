f78ebeb3780118a6b07a86b1997b340d57d15c4c
Jooaomarcelo <joaomdsb@hotmail.com>
2025-01-10 15:53:14 -0300

    added feature with global state usage with provider

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -5,9 +5,25 @@ import '../models/product.dart';
 
 class ProductList with ChangeNotifier {
   final List<Product> _items = dummyProducts;
+  bool _showFavoritesOnly = false;
 
   // Retornando uma cópia de itens e não a referência
-  List<Product> get items => [..._items];
+  List<Product> get items {
+    if (_showFavoritesOnly) {
+      return _items.where((item) => item.isFavorit).toList();
+    }
+    return [..._items];
+  }
+
+  void showFavoritesOnly() {
+    _showFavoritesOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoritesOnly = false;
+    notifyListeners();
+  }
 
   void addProduct(Product product) {
     _items.add(product);
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,15 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/product_list.dart';
 
 import '../components/product_grid.dart';
+
+enum FilterOptions {
+  favorites,
+  all,
+}
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Loja'),
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (ctx) => [
+              const PopupMenuItem(
+                value: FilterOptions.favorites,
+                child: Text('Favoritos'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.all,
+                child: Text('Todos'),
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.favorites) {
+                provider.showFavoritesOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+          ),
+        ],
       ),
       body: const ProductGrid(),
     );
