76434c1a156df62db27ed261271c31cf815b8a21
Jooaomarcelo <joaomdsb@hotmail.com>
2025-01-15 19:04:11 -0300

    added product item in products page

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../models/product_list.dart';
 
 import '../components/app_drawer.dart';
+import '../components/product_item.dart';
 
 class ProductsPage extends StatelessWidget {
   const ProductsPage({super.key});
@@ -11,19 +12,23 @@ class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ProductList products = Provider.of(context);
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Meus Produtos'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView.builder(
-          itemCount: products.itemsCount,
-          itemBuilder: (ctx, i) => Text(products.items[i].name),
-        ),
+        title: const Text('Gerenciar Produtos'),
       ),
       drawer: const AppDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: products.itemsCount,
+          itemBuilder: (ctx, i) => Column(
+            children: [
+              ProductItem(products.items[i]),
+              const Divider(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
